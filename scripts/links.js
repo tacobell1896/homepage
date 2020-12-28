8225a5e7e474891749732f419d712465a1915e26
Turner Bell <turner@tbell.us>
2020-12-28 18:51:02 -0500

    add more links

---
+++ scripts/links.js
@@ -0,0 +1,110 @@
+bored([
+    {
+        "display": "Reddit",
+        "url": "https://reddit.com"
+    },
+    {
+        "display": "4Chan",
+        "url": "https://boards.4chan.org"
+    },
+    {
+        "display": "Twitter",
+        "url": "https://twitter.com"
+    },
+    {
+        "display": "MonkeyType",
+        "url": "https://monkeytype.com"
+    }
+]);
+
+var productivity = [
+    {
+        "display": "Github",
+        "url": "https://github.com"
+    },
+    {
+        "display": "Unifi Controller",
+        "url": "https://unifi.ubnt.com"
+    },
+    {
+        "display": "Blackboard",
+        "url": "https://bblearn.tri-c.edu"
+    },
+    {
+        "display": "Code Academy",
+        "url": "https://codeacademy.com"
+    },
+    {
+        "display": "Free Code Camp",
+        "url": "https://freecodecamp.org"
+    },
+    {
+        "display": "Hacker101 CTF",
+        "url": "https://www.hacker101.com"
+    },
+    {
+        "display": "HackerOne",
+        "url": "https://www.hackerone.com"
+    }
+]
+
+var news = [
+    {
+        "display": "Hacker News",
+        "url": "https://news.ycombinator.com"
+    },
+    {
+        "display": "Ars Technica",
+        "url": "https://arstechnica.com"
+    },
+    {
+        "display": "The Verge",
+        "url": "https://theverge.com"
+    },
+    {
+        "display": "Polygon",
+        "url": "https://polygon.com"
+    },
+    {
+        "display": "Bleeping Computer",
+        "url": "https://bleepingcomputer.com"
+    },
+    {
+        "display": "Google News",
+        "url": "https://news.google.com"
+    }
+]
+
+var reference = [
+    {
+        "display": "Arch Wiki",
+        "url": "https://wiki.archlinux.org"
+    },
+    {
+        "display": "Wikipedia",
+        "url": "https://wikipedia.org"
+    },
+    {
+        "display": "Exploit Database",
+        "url": "https://exploit-db.com"
+    },
+    {
+        "display": "StackOverflow",
+        "url": "https://stackoverflow.com"
+    },
+    {
+        "display": "Mozilla Developer Network",
+        "url": "https://developer.mozilla.org"
+    }
+]
+
+var video = [
+    {
+        "display": "YouTube",
+        "url": "https://youtube.com"
+    },
+    {
+        "display": "Twitch",
+        "url": "https://twitch.tv"
+    }
+]
