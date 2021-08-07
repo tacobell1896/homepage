aa8f1b1bb8327b55629e52cf8ca49c16242ed8b1
Turner Bell <turner@tbell.us>
2021-08-07 10:50:01 -0400

    add listing

--- src/app/link-list.ts
+++ src/app/link-list.ts
@@ -4,13 +4,13 @@ export const LINKS: Link[] = [
     { name: 'Reddit', url: 'https://reddit.com', category: 'Bored'},
     { name: '4chan', url: 'https://boards.4chan.org', category: 'Bored'},
     { name: 'Twitter', url: 'https://twitter.com', category: 'Bored'},
-    { name: '', url: '', category: ''},
-    { name: '', url: '', category: ''},
-    { name: '', url: '', category: ''},
-    { name: '', url: '', category: ''},
-    { name: '', url: '', category: ''},
-    { name: '', url: '', category: ''},
-    { name: '', url: '', category: ''},
+    { name: 'MonkeyType', url: 'https://monkeytype.com', category: 'Bored'},
+    { name: 'MangaDex', url: 'https://mangadex.org', category: 'Bored'},
+    { name: 'Github', url: 'https://github.com', category: 'Productivity'},
+    { name: 'Unifi Controller', url: 'https://unifi.ubnt.com', category: 'Productivity'},
+    { name: 'AWS', url: 'https://aws.amazon.com', category: 'Productivity'},
+    { name: 'Free Code Camp', url: 'https://freecodecamp.org', category: ''},
+    { name: 'Hacker 101 CTF', url: 'https://www.hacker101.com', category: ''},
     { name: '', url: '', category: ''},
     { name: '', url: '', category: ''},
     { name: '', url: '', category: ''},
@@ -36,3 +36,29 @@ export const LINKS: Link[] = [
     { name: '', url: '', category: ''},
     { name: '', url: '', category: ''},
 ]
+/*					
+						"></a></li>
+						url: 'https://www.hackerone.com">HackerOne</a></li>
+						url: 'https://robinhood.com">Robinhood</a></li>
+						url: 'https://coinbase.com">Coinbase</a></li>
+						url: 'https://binance.us">Binance</a></li>
+						url: 'https://tdameritrade.com">TD Ameritrade</a></li>
+						url: 'https://us.etrade.com">E-Trade</a></li>
+						url: 'https://news.ycombinator.com">Hacker News</a></li>
+						url: 'https://arstechnica.com">Ars Technica</a></li>
+						url: 'https://theverge.com">The Verge</a></li>
+						url: 'https://polygon.com">Polygon</a></li>
+						url: 'https://bleepingcomputer.com">Bleeping Computer</a></li>
+						url: 'https://news.google.com">Google News</a></li>
+						url: 'https://wiki.archlinux.org">Arch Wiki</a></li>
+						url: 'https://www.wikipedia.org">Wikipedia</a></li>
+						url: 'https://exploit-db.com">Exploit Database</a></li>
+						url: 'https://stackoverflow.com">StackOverflow</a></li>
+						url: 'https://developer.mozilla.org">Mozilla Developer Network</a></li>
+						url: 'https://youtube.com">YouTube</a></li>
+						url: 'https://twitch.tv">Twitch</a></li>
+						url: 'https://crunchyroll.com">Crunchyroll</a></li>
+						url: 'https://netflix.com">Netflix</a></li>
+						url: 'https://hulu.com">Hulu</a></li>
+						url: 'https://www.amazon.com/Amazon-Video/b/?&node=2858778011">Prime Video</a></li>
+                        */
