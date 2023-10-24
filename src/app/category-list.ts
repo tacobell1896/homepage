d4ce0c53a82511386bb34076e4bc7158e98d0c25
Turner Bell <turner@tbell.us>
2023-10-24 03:41:58 +0000

    add separate lists

--- src/app/category-list.ts
+++ src/app/category-list.ts
@@ -1,3 +1,51 @@
-﻿import { Category, Link } from './category';
+﻿import { Category } from './category';
+import { LINKS } from './link-list'
 
-// export const CATEGORIES: 
+export const CATEGORIES: Category[] = [
+    {
+        id: 1, 
+        name: 'Bored', 
+        links: [   
+            
+        ]
+    },
+    {
+        id: 2, 
+        name: 'Productivity', 
+        links: [
+           
+        ]
+        
+    },
+    {
+        id: 3, 
+        name: 'Finance', 
+        links: [
+     
+        ]
+    },
+    {
+        id: 4, 
+        name: 'News', 
+        links: [
+    
+        
+        ]
+    },
+    {
+        id: 5, 
+        name: 'Reference', 
+        links: [
+    
+        
+        ]
+    },
+    {
+        id: 6, 
+        name: 'Video', 
+        links: [
+    
+        
+        ]
+    },
+]
--- src/app/category.ts
+++ src/app/category.ts
@@ -1,13 +1,7 @@
-﻿export interface Link {
+﻿ import { link } from "./link";
+ export interface Category {
     id: number;
     name: string;
-    url: string;
-    categoryId: number;
-  }
-  
-export interface Category {
-    id: number;
-    name: string;
-    links: Link[];
+    links: link[];
   }
   
